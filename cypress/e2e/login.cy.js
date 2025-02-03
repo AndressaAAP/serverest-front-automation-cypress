e77b12a434f240b6f4b484f152c9b3ac3d593daf
Andressa <andressati875@gmail.com>
2025-02-03 18:13:23 -0300

    :test_tube: test: Ajustando teste de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,11 +1,23 @@
 it( 'LOGIN', function(){
+    cy.api({
+        url: 'https://serverest.dev/usuarios',
+        method: 'POST',
+        failOnStatusCode: false,
+        body:{
+                "nome": "Maria Test",
+                "email": "maria_test@qa.com.br",
+                "password": "teste",
+                "administrador": "false"
+
+            }
+    })
     cy.visit('https://front.serverest.dev/login')
     cy.log('Acessou a página de login')
-    cy.get('input[type=email]').type('email@teste.com')
-    cy.get('input[type=password]').type('supersegura@123')
+    cy.get('input[type=email]').type('maria_test@qa.com.br')
+    cy.get('input[type=password]').type('teste')
     cy.get('button[type="submit"]').click()
 
     cy.get('section[class="row espacamento"]').should('be.visible')
 
-    cy.log('Teste')
+
 })
