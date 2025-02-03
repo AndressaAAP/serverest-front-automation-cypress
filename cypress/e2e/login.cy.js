1337b4b2e435dca9171cc50578bda3d6b96dba22
Andressa <andressapereira875@gmail.com>
2025-02-03 16:18:04 -0300

    :tada: Commit inicial

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,9 @@
+it( 'LOGIN', function(){
+    cy.visit('https://front.serverest.dev/login')
+    cy.log('Acessou a página de login')
+    cy.get('input[type=email]').type('email@teste.com')
+    cy.get('input[type=password]').type('supersegura@123')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('section[class="row espacamento"]').should('be.visible')
+})
